24f419008893e8940d36f3ea5fd9306a62bad9b6
glacier6 <1136730104@qq.com>
2020-12-04 16:58:25 +0800

    day1结束

---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id='home'>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners='banners'></home-swiper>
+    <home-recommend-view :recommends='recommends'></home-recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import {getHomeMultidata} from 'network/home'
+import HomeSwiper from 'views/home/childComps/HomeSwiper.vue'
+import HomeRecommendView from 'views/home/childComps/HomeRecommendView.vue'
+export default {
+  name:"Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView
+    },
+  data(){
+    return {
+      banners:[],
+      recommends:[]
+    }
+  },
+  created(){
+    getHomeMultidata().then(res => {
+      this.banners=res.data.banner.list
+      this.recommends=res.data.recommend.list
+    })
+  }
+}
+
+</script>
+
+<style scoped>
+  .home-nav{
+    background-color: var(--color-tint);
+    color:white;
+  }
+</style>
